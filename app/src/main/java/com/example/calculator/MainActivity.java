c65562c82e7b8e965f02b1e5de5ede9299bec72c
Vishal Kumar Singh <01vksingh@gmail.com>
2019-06-27 16:35:31 +0530

    addition working when variable name changed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,28 +7,33 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText n1;
     private EditText n2;
-    private Button b1;
-    private TextView t1;
-    private Button b2;
-    private Button b3;
-    private Button b4;
+    private Button add;
+    private TextView res;
+    private Button sub;
+    private Button div;
+    private Button mul;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        b1 = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_main);
+        add = (Button) findViewById(R.id.button);
         n1 = (EditText) findViewById(R.id.editText);
         n2 = (EditText) findViewById(R.id.editText2);
+        sub = (Button) findViewById(R.id.button2);
+        div = (Button) findViewById(R.id.button4);
+        mul = (Button) findViewById(R.id.button3);
 
-        t1 = (TextView) findViewById(R.id.textView);
+        res = (TextView) findViewById(R.id.textView);
 
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -37,9 +42,73 @@ public class MainActivity extends AppCompatActivity {
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 + val2;
-                    t1.setText(Double.toString(total));
+                    res.setText(Double.toString(total));
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(MainActivity.this,"Enter Numbers",Toast.LENGTH_LONG);
+                    toast.show();
                 }
             }
         });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
+                {
+                    double val1=Double.parseDouble(n1.getText().toString());
+                    double val2=Double.parseDouble(n2.getText().toString());
+                    double total= val1 - val2;
+                    res.setText(Double.toString(total));
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(MainActivity.this,"Enter Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
+                {
+                    double val1=Double.parseDouble(n1.getText().toString());
+                    double val2=Double.parseDouble(n2.getText().toString());
+                    double total= val1 * val2;
+                    res.setText(Double.toString(total));
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(MainActivity.this,"Enter Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
+                {
+                    double val1=Double.parseDouble(n1.getText().toString());
+                    double val2=Double.parseDouble(n2.getText().toString());
+                    double total= val1 / val2;
+                    res.setText(Double.toString(total));
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(MainActivity.this,"Enter Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
     }
 }
