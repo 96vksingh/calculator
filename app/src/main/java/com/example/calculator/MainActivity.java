53c88a175772add7759868c39d31a1ae4e95687b
Vishal Kumar Singh <01vksingh@gmail.com>
2019-06-28 10:18:16 +0530

    clear added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -20,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private Button sub;
     private Button div;
     private Button mul;
+    private Button clr;
     Animation animation;
     Animation animation2;
     Animation bblink;
+    Animation zzoom;
 
 
     @Override
@@ -30,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bblink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
+        zzoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         add = (Button) findViewById(R.id.button);
         n1 = (EditText) findViewById(R.id.editText);
         n2 = (EditText) findViewById(R.id.editText2);
         sub = (Button) findViewById(R.id.button2);
         div = (Button) findViewById(R.id.button4);
         mul = (Button) findViewById(R.id.button3);
+        clr = (Button) findViewById(R.id.button5);
         pro= (TextView) findViewById(R.id.textView2);
         res = (TextView) findViewById(R.id.textView);
 //        animation=AnimationUtils.loadAnimation(getApplicationContext,R.anim.sample.anim);
@@ -49,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
                     add.startAnimation(bblink);
+                    add.startAnimation(zzoom);
                     pro.setText("ADDITION");
+                    pro.startAnimation(bblink);
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 + val2;
@@ -71,7 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
                     sub.startAnimation(bblink);
+                    sub.startAnimation(zzoom);
                     pro.setText("SUBTRACTION");
+                    pro.startAnimation(bblink);
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 - val2;
@@ -93,7 +101,9 @@ public class MainActivity extends AppCompatActivity {
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
                     mul.startAnimation(bblink);
+                    mul.startAnimation(zzoom);
                     pro.setText("MULTIPLICATION");
+                    pro.startAnimation(bblink);
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 * val2;
@@ -115,7 +125,9 @@ public class MainActivity extends AppCompatActivity {
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
                     div.startAnimation(bblink);
+                    div.startAnimation(zzoom);
                     pro.setText("DIVISION");
+                    pro.startAnimation(bblink);
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 / val2;
@@ -126,6 +138,17 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(MainActivity.this,"Enter Numbers",Toast.LENGTH_LONG);
                     toast.show();
                 }
+            }
+        });
+
+        clr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1.setText("");
+                n2.setText("");
+                res.setText("");
+                pro.setText("");
+                n1.requestFocus();
             }
         });
 
