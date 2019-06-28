848757d976f888f1a8ddd07cab737c4d8ade4c0e
Vishal Kumar Singh <01vksingh@gmail.com>
2019-06-28 09:58:58 +0530

    animations added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
@@ -14,24 +16,31 @@ public class MainActivity extends AppCompatActivity {
     private EditText n2;
     private Button add;
     private TextView res;
+    private TextView pro;
     private Button sub;
     private Button div;
     private Button mul;
+    Animation animation;
+    Animation animation2;
+    Animation bblink;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bblink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
         add = (Button) findViewById(R.id.button);
         n1 = (EditText) findViewById(R.id.editText);
         n2 = (EditText) findViewById(R.id.editText2);
         sub = (Button) findViewById(R.id.button2);
         div = (Button) findViewById(R.id.button4);
         mul = (Button) findViewById(R.id.button3);
-
+        pro= (TextView) findViewById(R.id.textView2);
         res = (TextView) findViewById(R.id.textView);
-
+//        animation=AnimationUtils.loadAnimation(getApplicationContext,R.anim.sample.anim);
+        animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.sample_anim);
+        animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +48,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
+                    add.startAnimation(bblink);
+                    pro.setText("ADDITION");
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 + val2;
                     res.setText(Double.toString(total));
+                    res.startAnimation(animation);
                 }
                 else
                 {
@@ -58,10 +70,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
+                    sub.startAnimation(bblink);
+                    pro.setText("SUBTRACTION");
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 - val2;
                     res.setText(Double.toString(total));
+                    res.startAnimation(animation2);
                 }
                 else
                 {
@@ -77,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
+                    mul.startAnimation(bblink);
+                    pro.setText("MULTIPLICATION");
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 * val2;
@@ -97,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if((n1.getText().length()) > 0 && (n2.getText().length() > 0))
                 {
+                    div.startAnimation(bblink);
+                    pro.setText("DIVISION");
                     double val1=Double.parseDouble(n1.getText().toString());
                     double val2=Double.parseDouble(n2.getText().toString());
                     double total= val1 / val2;
